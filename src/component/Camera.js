92f5c35296608a1e87b8bb0636faf574c8a88cf2
Soojin6943 <sjhan0711@gmail.com>
2025-11-17 16:35:21 +0900

    feat(camera): 7초 뒤 자동으로 사진 한 장 촬영

--- src/component/Camera.js
+++ src/component/Camera.js
@@ -1,11 +1,49 @@
 import Webcam from "react-webcam";
 import './Camera.css';
+import { useRef, useCallback, useEffect, useState } from "react";
+
+const videoConstraints = {
+    width: 820,
+    height: 1180,
+    facingMode: "user"
+}
 
 export default function Camera() {
+
+    const webcamRef = useRef(null);
+    const [timeLeft, setTimeLeft] = useState(7*1000)
+
+    const capture = useCallback(
+        () => {
+            const imageSrc = webcamRef.current.getScreenshot();
+            console.log(imageSrc);
+        },
+        [webcamRef]
+    )
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft((prevTime) => prevTime - 1000);
+        }, 1000);
+
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            capture();
+            console.log("success");
+        }
+
+        return () => clearInterval(timer);
+    }, [timeLeft]);
+
     return (
         <>
             <div className="camera">
-                <Webcam />
+                <Webcam 
+                    audio={false} 
+                    screenshotFormat="image/jpeg" 
+                    ref={webcamRef}
+                    videoConstraints={videoConstraints} 
+                    mirrored={true} />
             </div>
         </>
     )
