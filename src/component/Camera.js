e39302c16a8f4c15f2354dabb15f353f10381995
Soojin6943 <sjhan0711@gmail.com>
2025-11-18 01:04:38 +0900

    feat(select): 사진 선택 화면에서 슬라이드 목록 구현

--- src/component/Camera.js
+++ src/component/Camera.js
@@ -1,7 +1,7 @@
 import Webcam from "react-webcam";
 import './Camera.css';
 import { useRef, useCallback, useEffect, useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const videoConstraints = {
     width: 820,
@@ -14,7 +14,7 @@ export default function Camera() {
     const webcamRef = useRef(null);
     const [timeLeft, setTimeLeft] = useState(7*1000);
     const [flash, setFlash] = useState(false);
-    const [potos, setPotos] = useState([]);
+    const [photos, setPhotos] = useState([]);
     const [count, setCount] = useState(0);
     const timeRef = useRef(null);
     const navigate = useNavigate();
@@ -25,7 +25,7 @@ export default function Camera() {
             setTimeout(() => setFlash(false), 150);
 
             const imageSrc = webcamRef.current.getScreenshot();
-            setPotos(prev => [...prev, imageSrc]);
+            setPhotos(prev => [...prev, imageSrc]);
             
             console.log("촬영됨");
         },
@@ -43,7 +43,7 @@ export default function Camera() {
 
         if (count >= 6) {
             clearInterval(timeRef.current);
-            navigate('/select');
+            navigate('/select', { state: { photos } });
         }
 
         if (timeLeft <= 0) {
@@ -67,7 +67,7 @@ export default function Camera() {
                 <div className="countdown">
                     {Math.ceil(timeLeft / 1000)}
                 </div>
-                <div className="poto-count">
+                <div className="photo-count">
                     {count} / 6
                 </div>
 
--- src/component/PhotoSelect.js
+++ src/component/PhotoSelect.js
@@ -1,5 +1,31 @@
+import { useLocation } from "react-router-dom";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css"; 
+import './PhotoSelect.css';
+
 export default function PhotoSelect() {
+    const location = useLocation();
+    const photos = location.state.photos;
+
+    const settings = {
+        dots: false,
+        arrows: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 4,
+        slidesToScroll: 1
+    };
+
     return (
-        <h1>사진 선택 화면</h1>
-    )
+        <div className="photo-select-wrapper">
+            <Slider {...settings}>
+                {photos.map((photo, index) => (
+                    <div >
+                        <img key={index} src={photo} width="160px" />
+                    </div>
+                ))}
+            </Slider>
+        </div>
+    );
 }
