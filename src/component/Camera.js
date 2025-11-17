fd58709ae76873c84006158864c00bde1abe29f3
Soojin6943 <sjhan0711@gmail.com>
2025-11-17 17:37:35 +0900

    feat(camera): 촬영 시 플래시 효과

--- src/component/Camera.js
+++ src/component/Camera.js
@@ -11,10 +11,14 @@ const videoConstraints = {
 export default function Camera() {
 
     const webcamRef = useRef(null);
-    const [timeLeft, setTimeLeft] = useState(7*1000)
+    const [timeLeft, setTimeLeft] = useState(7*1000);
+    const [flash, setFlash] = useState(false);
 
     const capture = useCallback(
         () => {
+            setFlash(true);
+            setTimeout(() => setFlash(false), 150);
+
             const imageSrc = webcamRef.current.getScreenshot();
             console.log(imageSrc);
         },
@@ -48,6 +52,8 @@ export default function Camera() {
                 <div className="countdown">
                     {Math.ceil(timeLeft / 1000)}
                 </div>
+
+                {flash && <div className="flash-effect"></div>}
             </div>
         </>
     )
