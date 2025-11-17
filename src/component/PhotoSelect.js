4992bbbd87c1803ade950877a4fcf0b6dc44e260
Soojin6943 <sjhan0711@gmail.com>
2025-11-18 01:44:37 +0900

    feat(select): 6컷 중 4컷 선택 저장 및 효과

--- src/component/PhotoSelect.js
+++ src/component/PhotoSelect.js
@@ -3,10 +3,13 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css"; 
 import './PhotoSelect.css';
+import { useState } from "react";
 
 export default function PhotoSelect() {
     const location = useLocation();
     const photos = location.state.photos;
+    const [selectedPhotos, setSelectedPhotos] = useState([]);
+    console.log(photos)
 
     const settings = {
         dots: false,
@@ -17,14 +20,43 @@ export default function PhotoSelect() {
         slidesToScroll: 1
     };
 
+    function photoClick(index) {
+        setSelectedPhotos(prev => {
+            if (prev.includes(index)) {
+                return prev.filter(i => i !== index);
+            }
+
+            if (prev.length > 3) {
+                alert("최대 4장까지만 선택할 수 있습니다.");
+                return prev;
+            }
+
+            return [...prev, index];
+        });
+        console.log("사진 선택됨" + index);
+    }
+
     return (
         <div className="photo-select-wrapper">
             <Slider {...settings}>
-                {photos.map((photo, index) => (
-                    <div >
-                        <img key={index} src={photo} width="160px" />
+                {photos.map((photo, index) => {
+                    const isSelected = selectedPhotos.includes(index);
+                    const order = selectedPhotos.indexOf(index) + 1;
+                    
+                    return (
+                    <div key={index} className="photo-wrapper">
+                        <img 
+                            src={photo} 
+                            width="160px" 
+                            className={`photo-img ${isSelected ? 'selected' : ''}`}
+                            onClick={() =>photoClick(index)}
+                        />
+
+                        {isSelected && (
+                            <div className="select-number">{order}</div>
+                        )}
                     </div>
-                ))}
+                )})}
             </Slider>
         </div>
     );
