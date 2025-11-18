1b38cd0a6c68d36571229acaca39b2a0350499a9
Soojin6943 <sjhan0711@gmail.com>
2025-11-19 00:08:29 +0900

    feat(home): 메인 요소 사이즈 확대

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 export default function Home() {
     return (
         <>
-            <h1>진토이즘</h1>
-            <Link to="/camera"><button>촬영하기</button></Link>
+            <h1 style={{ marginTop: '480px', fontSize: '60px' }}>진토이즘</h1>
+            <Link to="/camera"><button style={{ fontSize: '23px', border: 'none', padding: '5px 12px', borderRadius: '3px', backgroundColor: 'steelblue', color: 'white' }}>촬영하기</button></Link>
         </>
     )
 }
